49698785a059afab2a1823341303fd370f1d9120
Ryan Christian <ryanchristian4427@gmail.com>
2020-07-09 19:00:26 +0100

    test: Set up test suite

---
+++ tests/images/calendar.ts
@@ -0,0 +1,25 @@
+// Seriously, why does JS not have multi-line strings that don't insert tons of whitespace?
+
+export const processedLabels = [
+    '<text x="456" y="-7" class="month" style="fill: #000;">Mar</text>',
+    '<text text-anchor="start" class="wday" dx="-10" dy="8" style="fill: #000;">Mon</text>',
+];
+
+// prettier-ignore
+export const processedFooter =
+    '<div class="contrib-display">' +
+        '<span class="text-muted">Contributions in the last year</span>' +
+        '<span class="contrib-count">2,195 total</span>' +
+        '<span class="text-muted">Jul 10, 2019 - Jul 9, 2020</span>' +
+    '</div>';
+
+// prettier-ignore
+export const processedColors = [
+    '<li style="background-color: #ededed">',
+    '<li style="background-color: #62A197">',
+    '<li style="background-color: #428892">',
+    '<li style="background-color: #296887">',
+    '<li style="background-color: #253746">'
+];
+
+export const noScript = '<noscript>This component requires JS in order to function properly.</noscript>';
