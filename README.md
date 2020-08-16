8c4303b169b545ca58796b68d581659cbce90e0d
Ryan Christian <ryanchristian4427@gmail.com>
2020-08-16 23:24:42 +0100

    feat: Full rewrite, API greatly expanded

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ $ yarn add preact-github-calendar
 
 ## Usage
 
-```js
+```jsx
 import GitHubCalendar from "preact-github-calendar";
 
 export default function App() {
@@ -35,28 +35,61 @@ export default function App() {
 
 ## API
 
-This library offers a few options now, with more to come in the future. All are entirely optional, no need to provide the `options` prop if you have no wish to customize anything. These only override the defaults set by GitHub; if you like the display, no need to change anything!
+This library offers a few options now, with more to come in the future. All are entirely optional, no need to provide the `options` prop if you have no wish to customize anything. These only override the defaults; if you like the display, no need to change anything!
 
-### Calendar Class Name
+### blockMargin
+Type: `number`<br/>
+Default: `2`
+
+Sets the margin to be used between the blocks in the component. This affects both the vertical and horizontal margins.
+
+### blockSize
+Type: `number`<br/>
+Default: `12`
+
+Sets the size of the blocks in the component. This affects both the vertical and horizontal dimensions.
+
+### calendarClassName
 Type: `string`<br/>
 Default: `undefined`
 
-Sets the class of the root calendar element
+Sets the class name of the root calendar element.
 
-### Label Color
-Type: `<color>`
+### contributionColorArray
+Type: `['<color>', '<color>', '<color>', '<color>', '<color>']`<br/>
+Default: `['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39']`
+
+Changes the color spread in the calendar. The first item in the array replaces the color for least commits, and the last item replaces the color for most commits. Like `labelColor`, keywords, RGB(A) and HSL(A) values are all supported as valid colors. Default is GitHub's color scheme.
+
+### contributionCountFontSize
+Type: `number`<br/>
+Default: `26`
+
+Changes the font size of the total contribution count text. Takes any number.
+
+### fontSize
+Type: `number`<br/>
+Default: `11`
+
+Changes the font size of the less important text; the link to your profile, the date ranges, etc. Takes any number.
+
+### labelColor
+Type: `<color>`<br/>
+Default: `#000`
 
 Changes the font color of the month and days of the week labels. Takes any string representation of [`<color>`](https://developer.mozilla.org/en-US/docs/Web/CSS/color_value). Keywords, RGB(A) and HSL(A) values are all supported.
 
-### Show Week Days Label
-Type: `boolean`
+### labelFontSize
+Type: `number`<br/>
+Default: `14`
 
-Stop your calendar from adding week day labels to your graph. Enabled by default.
+Changes the font size of the month and days of the week labels. Takes any number.
 
-### Contribution Color Array
-Type: `['<color>', '<color>', '<color>', '<color>', '<color>']`
+### showWeekdaysLabels
+Type: `boolean`<br/>
+Default: `false`
 
-Changes the color spread in the calendar. The first item in the array replaces the color for least commits, and the last item replaces the color for most commits. Like `labelColor`, keywords, RGB(A) and HSL(A) values are all supported as valid colors.
+Add the week day labels to the left y-axis of your graph. Disabled by default.
 
 ## License
 
---
+++ src/Calendar/index.tsx
@@ -0,0 +1,248 @@
+import { VNode, Fragment, h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+
+const VERTICAL_SPACING = 1.5;
+const HORIZONTAL_SPACING = 2.4;
+
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+interface Contribution {
+    date: string;
+    count: number;
+    intensity: 0 | 1 | 2 | 3 | 4;
+}
+
+interface Options {
+    blockMargin?: number;
+    blockSize?: number;
+    calendarClassName?: string;
+    contributionColorArray?: [string, string, string, string, string];
+    contributionCountFontSize?: number;
+    fontSize?: number;
+    labelColor?: string;
+    labelFontSize?: number;
+    showWeekdaysLabels?: boolean;
+}
+
+export default function GitHubCalendar(props: { username: string; options?: Options }): VNode {
+    const {
+        blockMargin,
+        blockSize,
+        calendarClassName,
+        contributionColorArray,
+        contributionCountFontSize,
+        fontSize,
+        labelColor,
+        labelFontSize,
+        showWeekdaysLabels,
+    }: Options = {
+        blockMargin: 2,
+        blockSize: 12,
+        calendarClassName: undefined,
+        contributionColorArray: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
+        contributionCountFontSize: 24,
+        fontSize: 11,
+        labelColor: '#000',
+        labelFontSize: 14,
+        showWeekdaysLabels: false,
+        ...props.options,
+    };
+
+    const [graphData, setGraphData] = useState<{ total: number; contributions: Contribution[][] }>(null);
+    const [error, setError] = useState<string>('');
+
+    useEffect(() => {
+        async function getContributionData(): Promise<void> {
+            try {
+                const response = await (
+                    await fetch(`https://githubapi.ryanchristian.dev/user/${props.username}`)
+                ).json();
+                if (!('total' in response) && !('contributions' in response)) throw 'Invalid response data';
+                setGraphData(response);
+            } catch {
+                setError('Unknown Error');
+            }
+        }
+        getContributionData().then();
+    }, [props.username]);
+
+    function createWeekDayLabels(): JSX.Element[] {
+        if (showWeekdaysLabels) {
+            const weekDays = ['Mon', 'Wed', 'Fri'];
+            return weekDays.map((weekDay, i) => {
+                return (
+                    <text
+                        key={weekDay}
+                        x={0}
+                        y={
+                            labelFontSize * VERTICAL_SPACING +
+                            (blockSize + blockMargin) * ((i + 1) * 2 - 1) +
+                            (blockSize - 1)
+                        }
+                        style={{ fontSize: labelFontSize, fill: labelColor }}
+                    >
+                        {weekDay}
+                    </text>
+                );
+            });
+        }
+    }
+
+    function createMonthLabels(): JSX.Element[] {
+        const weeks: Contribution[] = [];
+        for (let i = 0; i < graphData.contributions.length - 1; i++) weeks.push(graphData.contributions[i][0]);
+        let previousMonth = 0;
+
+        return weeks.map((entry, i) => {
+            const month = new Date(entry.date).getMonth();
+            if (month !== previousMonth) {
+                previousMonth = month;
+                return (
+                    <text
+                        x={
+                            (showWeekdaysLabels ? (blockSize + blockMargin) * HORIZONTAL_SPACING : 0) +
+                            (blockSize + blockMargin) * i
+                        }
+                        y={labelFontSize}
+                        style={{ fontSize: labelFontSize, fill: labelColor }}
+                    >
+                        {MONTHS[month]}
+                    </text>
+                );
+            }
+        });
+    }
+
+    function createRects(): JSX.Element[] {
+        return graphData.contributions
+            .map((week) =>
+                week.map((day, y) => (
+                    <rect
+                        x="0"
+                        y={labelFontSize * VERTICAL_SPACING + (blockSize + blockMargin) * y}
+                        width={blockSize}
+                        height={blockSize}
+                        fill={contributionColorArray[day.intensity]}
+                        // data-tip={day.info ? getTooltipMessage(day as Required<Block>) : null}
+                        key={day.date}
+                    />
+                )),
+            )
+            .map((week, x) => (
+                <g
+                    key={x}
+                    transform={`translate(${
+                        (showWeekdaysLabels ? (blockSize + blockMargin) * HORIZONTAL_SPACING : 0) +
+                        (blockSize + blockMargin) * x
+                    }, 0)`}
+                >
+                    {week}
+                </g>
+            ));
+    }
+
+    function createLegend(): JSX.Element {
+        return (
+            <ul
+                style={{
+                    display: 'inline-block',
+                    margin: '0 5px',
+                    position: 'relative',
+                    paddingLeft: 0,
+                    bottom: '-1px',
+                }}
+            >
+                {contributionColorArray.map((color) => (
+                    <Fragment key={color}>
+                        <li
+                            style={{
+                                display: 'inline-block',
+                                width: blockSize,
+                                height: blockSize,
+                                backgroundColor: color,
+                            }}
+                        />{' '}
+                    </Fragment>
+                ))}
+            </ul>
+        );
+    }
+
+    function createDateRange(): string {
+        const maxWeekIndex = graphData.contributions.length - 1;
+        const lastYear = new Date(graphData.contributions[0][0].date);
+        const today = new Date(
+            graphData.contributions[maxWeekIndex][graphData.contributions[maxWeekIndex].length - 1].date,
+        );
+        return `${MONTHS[lastYear.getMonth()]} ${lastYear.getDate()}, ${lastYear.getFullYear()} - ${
+            MONTHS[today.getMonth()]
+        } ${today.getDate()}, ${today.getFullYear()}`;
+    }
+
+    return (
+        <Fragment>
+            {graphData !== null ? (
+                <figure class={calendarClassName}>
+                    <div style={{ padding: '.5rem' }}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox={`0 0 ${
+                                53 * (blockSize + blockMargin) -
+                                blockMargin +
+                                (showWeekdaysLabels ? (blockSize + blockMargin) * HORIZONTAL_SPACING : 0)
+                            } ${(blockSize + blockMargin) * 7 - blockMargin + labelFontSize * VERTICAL_SPACING}`}
+                        >
+                            {createWeekDayLabels()}
+                            {createMonthLabels()}
+                            {createRects()}
+                        </svg>
+                        <div
+                            style={{
+                                fontSize,
+                                overflowY: 'auto',
+                            }}
+                        >
+                            <div style={{ float: 'left' }}>
+                                Sum of pull requests, issues opened, and commits made by{' '}
+                                <a href={`https://github.com/${props.username}`} target="blank">
+                                    @{props.username}
+                                </a>
+                            </div>
+                            <div style={{ float: 'right' }}>
+                                Less
+                                {createLegend()}
+                                More
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        style={{
+                            padding: '15px 10px',
+                            textAlign: 'center',
+                            borderTop: '1px solid #ddd',
+                            fontSize: fontSize,
+                        }}
+                    >
+                        <span>Contributions in the last year</span>
+                        <span
+                            style={{
+                                fontWeight: '300',
+                                lineHeight: '1.3em',
+                                fontSize: contributionCountFontSize,
+                                display: 'block',
+                            }}
+                        >
+                            {graphData.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} Total
+                        </span>
+                        <span>{createDateRange()}</span>
+                    </div>
+                </figure>
+            ) : error ? (
+                <div style={{ textAlign: 'center' }}>{error}</div>
+            ) : (
+                <noscript>This component requires JS in order to function properly.</noscript>
+            )}
+        </Fragment>
+    );
+}
