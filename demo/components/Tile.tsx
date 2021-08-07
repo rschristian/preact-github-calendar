5743af9b8a459a038910418473be1dc22ca1a2aa
Ryan Christian <ryanchristian4427@gmail.com>
2021-08-06 23:10:46 -0500

    refactor: Rewriting docs site

--- demo/components/Tile.tsx
+++
@@ -1,5 +0,0 @@
-import { ComponentChild } from 'preact';
-
-export function Tile(props: { content: ComponentChild }) {
-    return <div class="mt-2 p-5 bg-white rounded shadow-lg">{props.content}</div>;
-}
